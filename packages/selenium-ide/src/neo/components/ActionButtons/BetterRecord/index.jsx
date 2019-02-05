9b2a78cc31eaa0b240dc649d47b18bc296e2bbea
Samuli Reijonen <reijonen.samuli@gmail.com>
2019-02-05 13:09:54 +0000

    Add superbot-record button

---
+++ packages/selenium-ide/src/neo/components/ActionButtons/BetterRecord/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import ActionButton from '../ActionButton'
+import './style.css'
+
+export default class BetterRecord extends React.Component {
+  static propTypes = {
+    isRecording: PropTypes.bool,
+    disabled: PropTypes.bool,
+    onClick: PropTypes.func,
+  }
+  render() {
+    return (
+      <div
+        className="record"
+        data-place='left'
+        data-tip={'<p>Record test</p>'}
+      >
+        <ActionButton
+          disabled={this.props.disabled}
+          isActive={this.props.isRecording}
+          onClick={this.props.onClick}
+          className="si-record"
+        />
+      </div>
+    )
+  }
+}
