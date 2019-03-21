6b10afe7fa56d0d058c40eaa028b694b4e1f36f4
Samuli Reijonen <reijonen.samuli@gmail.com>
2019-03-21 13:11:09 +0200

    Fix mode indicator

--- packages/selenium-ide/src/content/modeIndicator.js
+++ packages/selenium-ide/src/content/modeIndicator.js
@@ -1,52 +1,49 @@
 const addModeIndicator = (targetMode = null) => {
-  if(window.self !== window.top) return;
+  if(window.self !== window.top || document.getElementById('superbot-mode-indicator') !== null) return;
+
 
   const fragment = document.createDocumentFragment();
-  const indicator = document.createElement('div');
+  const indicatorContainer = document.createElement('iframe')
+  indicatorContainer.id = 'superbot-mode-indicator';
+  indicatorContainer.style.position = 'fixed';
+  indicatorContainer.style.bottom = '15px';
+  indicatorContainer.style.right = '15px';
+  indicatorContainer.style.width = '280px';
+  indicatorContainer.style.height = '80px';
+  indicatorContainer.style.border = '1px solid rgb(223, 223, 223)';
+  indicatorContainer.style.boxShadow = 'rgba(0, 0, 0, 0.3) 7px 7px 10px 0px';
+  indicatorContainer.style.zIndex = 2147483647;
+
   const recordingText = document.createElement('label');
   recordingText.innerText = 'Superbot IDE is recording...';
   recordingText.style.color = '#E80600';
   recordingText.style.marginTop = '10px';
   
   const modeText = document.createElement('label');
-  modeText.style.position = 'relative';
-  modeText.style.bottom = '-10px';
-  modeText.style.left = '0px';
+  modeText.style.position = 'absolute';
+  modeText.style.bottom = '20px';
+  modeText.style.left = '22px';
   modeText.innerText = 'Current mode: ' + (targetMode !== null ? targetMode : 'recording');
-  
-  indicator.id = 'superbot-mode-indicator';
-  
-  indicator.style.color = '#333';
-  indicator.style.fontSize = '16px';
-  indicator.style.fontFamily = 'Arial, Helvetica, sans-serif';
-  indicator.style.fontWeight = 'bold';
-  indicator.style.textAlign = 'center';
-
-  indicator.style.width = '280px';
-  indicator.style.height = '80px';
-  indicator.style.padding = '5px';
-  indicator.style.margin = 'initial';
-  indicator.style.zIndex = 2147483647;
-
-  indicator.style.position = 'fixed';
-  indicator.style.bottom = '36px';
-  indicator.style.right = '36px';
-
-  indicator.style.backgroundColor = 'whitesmoke';
-  indicator.style.border = '1px solid #dfdfdf';
-  indicator.style.borderRadius = '0px';
-  indicator.style['box-shadow'] = '7px   7px 10px 0 rgba(0,0,0,0.3)'
-
-  indicator.appendChild(recordingText);
-  indicator.appendChild(modeText);
-  fragment.appendChild(indicator)
+    
+  fragment.appendChild(indicatorContainer)
   document.body.appendChild(fragment);
+
+  const iframe = document.getElementById('superbot-mode-indicator')
+  iframe.contentDocument.body.appendChild(recordingText);
+  iframe.contentDocument.body.appendChild(modeText);
+  iframe.contentDocument.body.style.color = '#333';
+  iframe.contentDocument.body.style.fontSize = '16px';
+  iframe.contentDocument.body.style.fontFamily = 'Arial, Helvetica, sans-serif';
+  iframe.contentDocument.body.style.fontWeight = 'bold';
+  iframe.contentDocument.body.style.textAlign = 'center';
+  iframe.contentDocument.body.style.padding = '5px';
+  iframe.contentDocument.body.style.backgroundColor = 'whitesmoke';
   
   document.getElementById('superbot-mode-indicator').addEventListener(
     'mouseenter',() => {
-      indicator.style.visibility = 'hidden'
+      indicatorContainer.style.visibility = 'hidden'
       setTimeout(() => {
-        indicator.style.visibility = 'visible'
+        indicatorContainer.style.visibility = 'visible'
       }, 1000)
     },
     false
